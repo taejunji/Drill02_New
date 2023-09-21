f3202e5a04a60c319b08135444791f03380e49a0
taejunjiNote <qq00aa@naver.com>
2023-09-21 12:20:05 +0900

    Add render_frame funtion

--- character_grass.py
+++ character_grass.py
@@ -12,30 +12,30 @@ grass.draw_now(400,30)
 character.draw_now(400,90)
 delay(1)
 
+def render_frame(x,y):
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    character.draw_now(x,y)
+    delay(0.1)
+
 def run_circle():
     print('Circle')
     r,cx,cy = 200,400,300
     for deg in range(0,360,5):
         x = cx + r*math.cos(math.radians(deg))
         y = cy + r*math.sin(math.radians(deg))
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,y)
-        delay(0.1)
+        render_frame(x,y)
 
 def run_rectangle():
     print('Rectangle')
 
     #bottom Line
     for x in range(50,750+1,5):
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,90)
-        delay(0.1)
+      render_frame(x,y)  
 
 
 while True:
-    #run_circle()
+    run_circle()
     run_rectangle()
     break
 
