8a65d16156a57acbe1adc82d0a6cebd08f0db40a
taejunjiNote <qq00aa@naver.com>
2023-09-21 12:33:15 +0900

    Draw Left Line

--- character_grass.py
+++ character_grass.py
@@ -32,11 +32,13 @@ def run_rectangle():
     #bottom Line
     #for x in range(50,750+1,5):
       #render_frame(x,90)
-    for y in range(90,550+1,5):
-      render_frame(750,y)
+    #for y in range(90,550+1,5):
+      #render_frame(750,y)
     #top Line
     #for x in range(750,50-1,-5):
         #render_frame(x,550)
+    for y in range(550,90-1,-5):
+      render_frame(50,y)
 
 
 while True:
