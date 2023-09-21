8678c3646c3db550d0fb6ac6f3ebe3b588645023
taejunjiNote <qq00aa@naver.com>
2023-09-21 11:42:34 +0900

    boy print complete

--- character_grass.py
+++ character_grass.py
@@ -3,5 +3,12 @@ from pico2d import *
 open_canvas()
 
 # fill here
+character = load_image('character.png')
+grass = load_image('grass.png')
+
+clear_canvas_now()
+grass.draw_now(400,30)
+character.draw_now(400,90)
+delay(1)
 
 close_canvas()
